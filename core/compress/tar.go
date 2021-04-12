8e57e32aa140c94c35c4917d317639acac892a2e
dianjiu <dianjiu@dianjiu.cc>
2021-04-12 17:37:54 +0800

    新增tar 递归压缩
    Signed-off-by: dianjiu <dianjiu@dianjiu.cc>

--- core/compress/tar.go
+++ core/compress/tar.go
@@ -5,7 +5,10 @@ import (
 	"compress/gzip"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 // Tar Compressed file.
@@ -66,8 +69,83 @@ func Tar(src, dest string) (err error) {
 			return err
 		}
 	}
-	fmt.Println("tar.gz ok")
+	fmt.Println("Tar Success")
 	return nil
+}
+
+// Tar Compressed file.
+// src is the abbreviation of source, which is a directory of files to be compressed.
+// dst is the abbreviation of destination, which is the compressed file directory.
+func TarRecursive(src, dest string) (err error) {
+	// Create a file to be written.
+	fw, err := os.Create(dest)
+	defer fw.Close()
+	if err != nil {
+		return err
+	}
+
+	// Create zip.Write through fw.
+	gw := gzip.NewWriter(fw)
+	defer func() {
+		// Check if it is successfully closed.
+		if err := gw.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// tar write
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+
+	// Write the file to zw, because there may be many directories and files, so recursive processing.
+	return filepath.Walk(src, func(path string, fi os.FileInfo, errBack error) (err error) {
+		if errBack != nil {
+			return errBack
+		}
+
+		// Create zip file information through file information.
+		fh, err := tar.FileInfoHeader(fi, "")
+		if err != nil {
+			return
+		}
+
+		// Replace the file name in the file information
+		fh.Name = strings.TrimPrefix(path, string(filepath.Separator))
+
+		// Judge it is not a directory.
+		if fi.IsDir() {
+			fh.Name += "/"
+		}
+
+		// 信息头
+		h := new(tar.Header)
+		h.Name = fi.Name()
+		h.Size = fi.Size()
+		h.Mode = int64(fi.Mode())
+		h.ModTime = fi.ModTime()
+		// 写信息头
+		err = tw.WriteHeader(h)
+		if err != nil {
+			return err
+		}
+
+		// Open the file to be compressed.
+		fr, err := os.Open(path)
+		defer fr.Close()
+		if err != nil {
+			return
+		}
+
+		// Copy the opened file to w .
+		_, err = io.Copy(tw, fr)
+		if err != nil {
+			return
+		}
+		// Output compressed content.
+		fmt.Printf("TarRecursive Success \n")
+
+		return nil
+	})
 }
 
 // UnTar decompressed file.
@@ -110,6 +188,6 @@ func UnTar(src, dest string) (err error) {
 			return err
 		}
 	}
-	fmt.Println("un tar.gz ok")
+	fmt.Println("UnTar Success!")
 	return nil
 }
