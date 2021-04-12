6160cc16439cdc5b11c34747ad5ccd601632fcc0
dianjiu <dianjiu@dianjiu.cc>
2021-04-12 10:36:11 +0800

    优化关闭流

--- core/compress/zip.go
+++ core/compress/zip.go
@@ -10,130 +10,145 @@ import (
 	"strings"
 )
 
-// Zip 压缩文件
+// Zip Compressed file.
+// dst is the file directory to be compressed.
+// src is the file directory after successful compression.
 func Zip(dst, src string) (err error) {
-	// 创建准备写入的文件
+	// Create a file to be written.
 	fw, err := os.Create(dst)
 	defer fw.Close()
 	if err != nil {
 		return err
 	}
 
-	// 通过 fw 来创建 zip.Write
+	// Create zip.Write through fw.
 	zw := zip.NewWriter(fw)
 	defer func() {
-		// 检测一下是否成功关闭
+		// Check if it is successfully closed.
 		if err := zw.Close(); err != nil {
 			log.Fatalln(err)
 		}
 	}()
 
-	// 下面来将文件写入 zw ，因为有可能会有很多个目录及文件，所以递归处理
+	// Write the file to zw, because there may be many directories and files, so recursive processing.
 	return filepath.Walk(src, func(path string, fi os.FileInfo, errBack error) (err error) {
 		if errBack != nil {
 			return errBack
 		}
 
-		// 通过文件信息，创建 zip 的文件信息
+		// Create zip file information through file information.
 		fh, err := zip.FileInfoHeader(fi)
 		if err != nil {
 			return
 		}
 
-		// 替换文件信息中的文件名
+		// Replace the file name in the file information
 		fh.Name = strings.TrimPrefix(path, string(filepath.Separator))
 
-		// 这步开始没有加，会发现解压的时候说它不是个目录
+		// Judge it is not a directory.
 		if fi.IsDir() {
 			fh.Name += "/"
 		}
 
-		// 写入文件信息，并返回一个 Write 结构
+		// Write file information and return a Write structure.
 		w, err := zw.CreateHeader(fh)
 		if err != nil {
 			return
 		}
 
-		// 检测，如果不是标准文件就只写入头信息，不写入文件数据到 w
-		// 如目录，也没有数据需要写
+		// If it is not a standard file, only the header information is written, and the file data is not written to w.
+		// Such as a directory, there is no data to write.
 		if !fh.Mode().IsRegular() {
 			return nil
 		}
 
-		// 打开要压缩的文件
+		// Open the file to be compressed.
 		fr, err := os.Open(path)
 		defer fr.Close()
 		if err != nil {
 			return
 		}
 
-		// 将打开的文件 Copy 到 w
+		// Copy the opened file to w .
 		n, err := io.Copy(w, fr)
 		if err != nil {
 			return
 		}
-		// 输出压缩的内容
-		fmt.Printf("Gotool成功压缩文件： %s, 共写入了 %d 个字符的数据\n", path, n)
+		// Output compressed content.
+		fmt.Printf("Zip success %s, A total of %d characters of data are written\n", path, n)
 
 		return nil
 	})
 }
 
-// UnZip 解压缩
+// UnZip decompressed file.
 func UnZip(dst, src string) (err error) {
-	// 打开压缩文件，这个 zip 包有个方便的 ReadCloser 类型
-	// 这个里面有个方便的 OpenReader 函数，可以比 tar 的时候省去一个打开文件的步骤
+	// Open compressed file.
 	zr, err := zip.OpenReader(src)
 	defer zr.Close()
 	if err != nil {
 		return
 	}
 
-	// 如果解压后不是放在当前目录就按照保存目录去创建目录
+	// If it is not placed in the current directory after decompression, create a directory according to the save directory.
 	if dst != "" {
 		if err := os.MkdirAll(dst, 0755); err != nil {
 			return err
 		}
 	}
 
-	// 遍历 zr ，将文件写入到磁盘
+	// Traverse zr and write the file to disk
 	for _, file := range zr.File {
 		path := filepath.Join(dst, file.Name)
 
-		// 如果是目录，就创建目录
+		// If it is a directory, create a directory.
 		if file.FileInfo().IsDir() {
 			if err := os.MkdirAll(path, file.Mode()); err != nil {
 				return err
 			}
-			// 因为是目录，跳过当前循环，因为后面都是文件的处理
+			// Because it is a directory, skip the current loop, because the following is the processing of files.
 			continue
 		}
 
-		// 获取到 Reader
+		// Get the Reader.
 		fr, err := file.Open()
 		if err != nil {
+			//Also close when abnormal
+			close(fr, nil)
 			return err
 		}
 
-		// 创建要写出的文件对应的 Write
+		// Create the Write corresponding to the file to be written.
 		fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
 		if err != nil {
+			//Also close when abnormal
+			close(fr, fw)
 			return err
 		}
 
 		n, err := io.Copy(fw, fr)
 		if err != nil {
+			//Also close when abnormal
+			close(fr, fw)
 			return err
 		}
 
-		// 将解压的结果输出
-		fmt.Printf("Gotool成功解压 %s ，共写入了 %d 个字符的数据\n", path, n)
+		// Output the decompressed result.
+		fmt.Printf("UnZip success %s A total of %d characters have been written\n", path, n)
 
-		// 因为是在循环中，无法使用 defer ，直接放在最后
-		// 不过这样也有问题，当出现 err 的时候就不会执行这个了，
-		// 可以把它单独放在一个函数中，这里是个实验，就这样了
-		fw.Close()
-		fr.Close()
+		//Finally remember to close
+		close(fr, fw)
 	}
 	return nil
+}
+
+// close Close file stream.
+func close(fr io.ReadCloser, fw *os.File) {
+	if fr != nil {
+		fr.Close()
+	}
+	if fw != nil {
+		fw.Close()
+	}
+
 }
